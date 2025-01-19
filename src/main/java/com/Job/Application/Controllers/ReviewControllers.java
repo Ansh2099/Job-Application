c1e5b5833e0331189f5d2aa8b1d6a12ea02f8be0
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-01-19 16:20:36 +0530

    Changes

--- src/main/java/com/Job/Application/Controllers/ReviewControllers.java
+++ src/main/java/com/Job/Application/Controllers/ReviewControllers.java
@@ -1,4 +1,67 @@
 package com.Job.Application.Controllers;
 
+import com.Job.Application.Model.Jobs;
+import com.Job.Application.Model.Reviews;
+import com.Job.Application.Service.ReviewService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class ReviewControllers {
+
+    @Autowired
+    private ReviewService service;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Reviews>> getAllReviews() {
+        return new ResponseEntity<>(service.getAllReviews(), HttpStatus.FOUND);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reviews> getReviewById(@PathVariable Long id){
+        Reviews review = service.getReviewById(id);
+        if (review != null)
+            return new ResponseEntity<>(review, HttpStatus.FOUND);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> postReview(@Valid @RequestBody Reviews review){
+        try{
+            return new ResponseEntity<>(service.postReview(review), HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteReview(@PathVariable long id){
+        Reviews review = service.getReviewById(id);
+        if (review != null){
+            service.deleteReview(id);
+            return new ResponseEntity<>(HttpStatus.FOUND);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateReview(@Valid @RequestPart Reviews review, @RequestPart Long id){
+        try {
+            Reviews review1 = service.getReviewById(id);
+            if (review1 != null)
+                return new ResponseEntity<>(service.updateReview(review, id), HttpStatus.FOUND);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/Job/Application/Service/ReviewService.java
+++ src/main/java/com/Job/Application/Service/ReviewService.java
@@ -1,4 +1,36 @@
 package com.Job.Application.Service;
 
+import com.Job.Application.Model.Reviews;
+import com.Job.Application.Repo.ReviewRepo;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReviewService {
+
+    @Autowired
+    private ReviewRepo repo;
+
+    public List<Reviews> getAllReviews() {
+        return repo.findAll();
+    }
+
+    public Reviews postReview(Reviews review) {
+        return repo.save(review);
+    }
+
+    public void deleteReview(Long id) {
+        repo.deleteById(id);
+    }
+
+    public Reviews getReviewById(long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Object updateReview(@Valid Reviews review, Long id) {
+        return repo.save(review);
+    }
 }
