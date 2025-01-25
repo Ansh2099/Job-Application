c76ae321078fec44a18122939c02bc37d488355e
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-01-25 09:00:41 +0530

    Changes

--- src/main/java/com/Job/Application/Repo/CompanyRepo.java
+++ src/main/java/com/Job/Application/Repo/CompanyRepo.java
@@ -2,8 +2,15 @@ package com.Job.Application.Repo;
 
 import com.Job.Application.Model.Companies;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CompanyRepo extends JpaRepository<Companies, Long> {
+    @Query("SELECT p from Companies p WHERE " +
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.desc) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Companies> searchByKeyword(String keyword);
 }
