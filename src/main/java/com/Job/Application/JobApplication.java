68ed45d3dce910d9f0ad107b1b337ff5d11c13e8
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-03-06 12:57:38 +0530

    Added more dependencies, and defined property source

--- src/main/java/com/Job/Application/JobApplication.java
+++ src/main/java/com/Job/Application/JobApplication.java
@@ -2,12 +2,13 @@ package com.Job.Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(name = "dotenv", value = "classpath:Config/.env", ignoreResourceNotFound = true)
 public class JobApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobApplication.class, args);
 	}
-
 }
