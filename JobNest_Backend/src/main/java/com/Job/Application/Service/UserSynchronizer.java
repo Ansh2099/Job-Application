ef9fb655e0c2f6b5c1dcb4e8bda172cc66f056be
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-04-24 18:56:18 +0530

    Minor import changes to UserSynchronizer file

--- JobNest_Backend/src/main/java/com/Job/Application/Service/UserSynchronizer.java
+++ JobNest_Backend/src/main/java/com/Job/Application/Service/UserSynchronizer.java
@@ -6,7 +6,6 @@ import com.Job.Application.Repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.web.webauthn.management.UserCredentialRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -23,39 +22,39 @@ public class UserSynchronizer {
     public void synchronizeWithIdp(Jwt token) {
         log.info("Synchronizing user with idp");
         Map<String, Object> claims = token.getClaims();
-        
+
         // Check if sub claim exists (this is the user ID from Keycloak)
         if (claims.containsKey("sub")) {
             String userId = claims.get("sub").toString();
             log.info("Synchronizing user with ID {}", userId);
-            
+
             // Create user from token attributes
             User user = userMapper.fromTokenAttributes(claims);
-            
+
             // Try to find existing user by ID first
             Optional<User> existingUserById = userRepository.findById(userId);
-            
+
             // If user exists by ID, update their data
             if (existingUserById.isPresent()) {
                 User existingUser = existingUserById.get();
                 user.setId(existingUser.getId());
                 user.setCreatedDate(existingUser.getCreatedDate());
                 log.info("Updating existing user with ID {}", userId);
-            } 
+            }
             // If user doesn't exist by ID but has email, try to find by email
             else if (claims.containsKey("email")) {
                 String userEmail = claims.get("email").toString();
                 Optional<User> existingUserByEmail = userRepository.findByEmail(userEmail);
-                
+
                 if (existingUserByEmail.isPresent()) {
                     User existingUser = existingUserByEmail.get();
                     // Update ID to match Keycloak ID
-                    user.setId(userId);
+                    user.setId(Long.valueOf(userId));
                     user.setCreatedDate(existingUser.getCreatedDate());
                     log.info("Updating existing user with email {} to have ID {}", userEmail, userId);
                 }
             }
-            
+
             // Save the user regardless
             userRepository.save(user);
             log.info("User synchronization completed for ID {}", userId);
