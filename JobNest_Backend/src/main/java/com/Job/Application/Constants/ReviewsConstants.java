b37bd5bca15eee9615d86798137c4a63a859e1d2
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-04-26 09:49:46 +0530

    Added more Constants Files

---
+++ JobNest_Backend/src/main/java/com/Job/Application/Constants/ReviewsConstants.java
@@ -0,0 +1,7 @@
+package com.Job.Application.Constants;
+
+public class ReviewsConstants {
+    public static final String FindByCompanyId = "Reviews.FindByCompanyId";
+    public ReviewsConstants() {
+    }
+}
---
+++ JobNest_Backend/src/main/java/com/Job/Application/Constants/UserConstants.java
@@ -0,0 +1,10 @@
+package com.Job.Application.Constants;
+
+public class UserConstants {
+    public static final String FindByEmail = "User.FindByEmail";
+    public static final String FindByUsername = "User.FindByUsername";
+    public static final String ExistsByEmail = "User.ExistsByEmail";
+
+    public UserConstants() {
+    }
+}
